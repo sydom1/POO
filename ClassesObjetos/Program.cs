a8f7f9ae230e5f69850b81fec01d9ccc752dd9f2
Sydom1 <sydom2013@gmail.com>
2022-11-11 21:59:52 -0300

    Adicionar arquivos de projeto.

---
+++ ClassesObjetos/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesObjetos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Minhaclasse mClasse = new Minhaclasse();
+            Minhaclasse m2 = null; /* null e uma classe vazia*/
+
+            OutraClasse outra = new OutraClasse();
+        }
+    }
+
+    class Minhaclasse
+    {
+
+    }
+}
